df8958f24b12debcf95e4db6e966fac1ef178cf9
Anrin David Ryan <anrin11@gmail.com>
2015-04-06 16:51:21 +0530

    Update core.js

--- core.js
+++ core.js
@@ -127,13 +127,12 @@ var core = exports.core = {
 			if (args === 'title') return '<div class="profile-title">&nbsp;' + info + '</div>';
 			if (args === 'bp') return '<br>&nbsp;<strong><font color="' + this.color + '">Battle Points:</font></strong>&nbsp;' + info;
 			if (args === 'tourWins') return '<br>&nbsp;<strong><font color="' + this.color + '">Tournament Wins:</font></strong>&nbsp;' + info;
-			if (args === 'pclWins') return '<br>&nbsp;<strong><font color="' + this.color + '">PCL Tournament Wins:</font></strong>&nbsp;' + info;
 		},
 
 	},
 
 	ladder: function (limit) {
-		var data = fs.readFileSync('config/tourWins.csv', 'utf-8');
+		var data = fs.readFileSync('config/tourWins.csv', 'gmt+5:30');
 		var row = ('' + data).split("\n");
 
 		var list = [];
@@ -165,8 +164,8 @@ var core = exports.core = {
 		return 0;
 	},
 
-	pclLadder: function (limit) {
-		var data = fs.readFileSync('config/pclWins.csv', 'utf-8');
+	ladder: function (limit) {
+		var data = fs.readFileSync('config/tourWins.csv', 'gmt+5:30');
 		var row = ('' + data).split("\n");
 
 		var list = [];
@@ -201,8 +200,8 @@ var core = exports.core = {
 	shop: function (showDisplay) {
 		var shop = [
 			['Star', 'Buy a \u2606 to go in front of your name and puts you at the top of the user list. (Goes away if you leave for more than one hour or the server restarts.)', 5],
-			['Fix', 'Buy the ability to alter your current custom avatar. (Don\'t buy this if you don\'t have a custom avatar. If you have a custom avatar and would like to apply it to other usernames, contact the admin "wolf" and don\'t buy this.)', 25],
-			['Title', 'Buy a user title for your profile. (Can be seen via "/profile username". Check "/profile wolf" for an example.)', 30],
+			['Fix', 'Buy the ability to alter your current custom avatar. (Don\'t buy this if you don\'t have a custom avatar. If you have a custom avatar and would like to apply it to other usernames, contact the admin "anrin" and don\'t buy this.)', 25],
+			['Title', 'Buy a user title for your profile. (Can be seen via "/profile username". Check "/profile Anrin Dео Aνіs" for an example.)', 30],
 			['Poof', 'Buy a poof message to be added into your pool of possible poofs. Poofs are custom leave messages.', 35],
 			['Avatar', 'Buy a custom avatar to be applied to your name. (You supply. Images larger than 80x80 may not show correctly.)', 50]
 		];
